dcdbc3a8e83d1b3daa8a6ecf9045facff289af82
atakanyuksell <atakanyukselis@hotmail.com>
2024-10-09 17:23:10 +0300

    #1 Değişikliklerden sonra

--- obj/Release/net6.0/WheatherAppSol.AssemblyInfo.cs
+++ obj/Release/net6.0/WheatherAppSol.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WheatherAppSol")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68f06d0e101d057497ca8d755c4d573a92b251cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WheatherAppSol")]
